0db7ecb23c0ac10c8e7c069b15a8cc46eb9bcc25
Alexandre Constantino <dreaming.about.electric.sheep@gmail.com>
2016-05-23 00:57:54 +0100

    Add exuberant-ctags to list of packages

--- provision/20_packages.sh
+++ provision/20_packages.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 # GNU/Linux survival kit
-apt-get install -y vim-nox htop tree ncdu
-apt-get install -y tmux byobu ccze --no-install-recommends
-
-# optimize environment
+apt-get install -y vim-nox
+apt-get install -y htop tree ncdu tmux byobu ccze --no-install-recommends
 apt-get install -y zsh
 sed -i.BAK 's#/bin/bash#/usr/bin/zsh#g' /etc/passwd
-apt-get install -y git
+apt-get install -y git exuberant-ctags
 apt-get install -y ack silversearcher-ag --no-install-recommends
 apt-get install -y stow --no-install-recommends
 apt-get install -y msort --no-install-recommends
-
 
 
 # developer things
